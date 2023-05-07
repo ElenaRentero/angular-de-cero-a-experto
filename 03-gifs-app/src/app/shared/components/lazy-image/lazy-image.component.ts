7943757604884fb9d037e9f40a2f2c3e014a7f54
Elena Rentero <elenaclementerentero@gmail.com>
2023-05-07 17:50:11 +0200

    Actualizar link gifsApp y README.md

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
-  templateUrl: './lazy-image.component.html',
-  styleUrls: ['./lazy-image.component.css']
+  templateUrl: './lazy-image.component.html'
 })
 export class LazyImageComponent implements OnInit {
   @Input()
